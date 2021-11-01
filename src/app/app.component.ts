8da26769e275bec01f31b9a9e9748872687dc40c
b45i <amalshajan2011@gmail.com>
2021-11-01 17:22:02 +0530

    dunamic facicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'dynamicFav';
+  set favIcon(val: string) {
+    this._document.getElementById('appFavicon')?.setAttribute('href', val);
+  }
+
+  url = '';
+
+  constructor(@Inject(DOCUMENT) private _document: HTMLDocument) {}
+
+  onButtonClick() {
+    this.favIcon = this.url;
+  }
 }
